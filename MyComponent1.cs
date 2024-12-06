c6508639dd2b63eb72d666a087aec7724c1cc81d
johnmirts <johnmirts@hotmail.com>
2024-12-06 14:51:54 -0500

    [add] test GH component

---
+++ MyComponent1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Grasshopper.Kernel;
+
+namespace IsoVistGH
+{
+    public class MyComponent1 : BaseComponent
+    {
+        /// <summary>
+        /// Initializes a new instance of the MyComponent1 class.
+        /// </summary>
+        public MyComponent1()
+          : base("MyComponent1",
+                 "Nickname",
+                 "Description",
+                 GH_Exposure.primary,
+                 Properties.Resources.icon_question)
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager pManager) {
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager) {
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA) {
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid {
+            get { return new Guid("F99E201D-4C47-48DF-B597-706DA125FD8A"); }
+        }
+    }
+}
