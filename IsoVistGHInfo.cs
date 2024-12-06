7ab18e7231c1b2352deed073097c89b170c31b32
johnmirts <johnmirts@hotmail.com>
2024-12-06 13:42:04 -0500

    IsoVist info file creation

---
+++ IsoVistGHInfo.cs
@@ -0,0 +1,48 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace IsoVistGH {
+    public class IsoVistGHInfo : GH_AssemblyInfo {
+        public override string Name {
+            get { 
+                return "IsoVistGH";
+            }
+        }
+
+        public override string Version {
+            get {
+                return "version 0.0.1";
+            }
+        }
+
+        public override Bitmap Icon {
+            get {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description {
+            get {
+                //Return a short string describing the purpose of this GHA library.
+                return "The implementation of an IsoVist algorithm for 2D applications within McNeel Rhinoceros and Grasshopper";
+            }
+        }
+
+        public override Guid Id => new Guid("a0b7200f-2ce3-4f37-876d-086600e64f5b");
+
+        public override string AuthorName {
+            get {
+                //Return a string identifying you or your company.
+                return "Dr.sc. Ioannis Mirtsopoulos";
+            }
+        }
+        public override string AuthorContact {
+            get {
+                //Return a string representing your preferred contact details.
+                return "ioannis@mirtsopoulos.xyz";
+            }
+        }
+    }
+}
