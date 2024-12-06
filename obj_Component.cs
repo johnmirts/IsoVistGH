1d16025bc3382eaa877587dcbde0a6cd6140b82f
johnmirts <johnmirts@hotmail.com>
2024-12-06 14:25:01 -0500

    [add] component abstract class

---
+++ obj_Component.cs
@@ -0,0 +1,42 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Attributes;
+using System.Drawing;
+
+namespace IsoVistGH {
+    public abstract class BaseComponent : GH_Component {
+
+        private readonly GH_Exposure exposure = GH_Exposure.hidden;
+        private readonly Bitmap icon = null;
+
+        public BaseComponent(string _componentName, string _nickname, string _description, string _subcategory, GH_Exposure _exposure, Bitmap _icon)
+          : base(_componentName, _nickname, _description, "IsoVistGH", "Standard") {
+            exposure = _exposure;
+            icon = _icon;
+            IconDisplayMode = GH_IconDisplayMode.icon;
+        }
+
+        protected override void BeforeSolveInstance() {
+            base.BeforeSolveInstance();
+            var plugin = new IsoVistGHInfo();
+            Message = plugin.Version;
+        }
+
+        public override void CreateAttributes() {
+            base.m_attributes = new BaseComponentAttributes(this);
+        }
+
+        public override GH_Exposure Exposure {
+            get { return exposure; }
+        }
+
+        protected override Bitmap Icon {
+            get {
+                return icon;
+            }
+        }
+    }
+    internal class BaseComponentAttributes : GH_ComponentAttributes {
+        internal BaseComponentAttributes(IGH_Component component)
+          : base(component) { }
+    }
+}
